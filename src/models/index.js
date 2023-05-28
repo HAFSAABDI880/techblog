a29e31bb9883b4b6a46001d75018d4f36b6c0147
hafsaabdi880 <hafsaabdi88@outlook.com>
2023-05-28 20:07:57 +0100

    models

---
+++ src/models/index.js
@@ -0,0 +1,31 @@
+const User = require("./user");
+const Blog = require("./Blog");
+const Comment = require("./comment");
+
+Blog.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+User.hasMany(Blog, {
+  foreignKey: "userId",
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: "blogId",
+  onDelete: "CASCADE",
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: "blogId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "userId",
+});
+
+module.exports = { User, Blog, Comment };
