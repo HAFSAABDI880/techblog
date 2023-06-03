83dc6882e954e2ecda6cb727c6fb04715278b0b8
hafsaabdi880 <hafsaabdi88@outlook.com>
2023-06-03 23:52:24 +0100

    https://github.com/HAFSAABDI880/techblog

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,20 +1,9 @@
-require("dotenv").config();
-const Sequelize = require("sequelize");
-
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-
-const options = {
-  host: DB_HOST,
-  dialect: "mysql",
-  port: 3306,
-  logging: false,
-};
-
-const connection = process.env.JAWSDB_URL
+require('dotenv').config();
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, options);
-
-module.exports = connection;
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
